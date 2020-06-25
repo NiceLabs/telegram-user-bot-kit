1ad2d0707c56e4ac4cd03a2271b41a5a1b406448
Septs <github@septs.pw>
2020-06-26 07:28:06 +0800

    Regular update: 2020-06-25T23:28:06Z

--- user_bot_kit/__init__.py
+++ user_bot_kit/__init__.py
@@ -12,13 +12,13 @@ def retry(func):
             print(repr(err))
             if err.ID == "PEER_ID_INVALID":
                 return
-            func(*args, **kwargs)
+            wrapper(*args, **kwargs)
         except FloodWait as err:
             end_date = datetime \
                 .fromtimestamp(time.time() + err.x) \
                 .isoformat()
             print("Sleeping for %ds (%s)" % (err.x, end_date))
             time.sleep(err.x)
-            func(*args, **kwargs)
+            wrapper(*args, **kwargs)
 
     return wrapper
