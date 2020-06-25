f32adb7397b64a9cc22dbafab9d3524dbcafe1bb
Septs <github@septs.pw>
2020-06-26 07:14:04 +0800

    Regular update: 2020-06-25T23:14:04Z

--- export-history.py
+++ export-history.py
@@ -8,7 +8,6 @@ from pyrogram import ChatMember, Client, Message
 import user_bot_kit
 
 app = Client("my_account")
-enable_get_bio = True
 
 
 def export_members(chat_id: int):
@@ -18,7 +17,7 @@ def export_members(chat_id: int):
         member: ChatMember
         if index % 100 == 0 or index % round(count / 20) == 0:
             print("# {:>6d} / {:<6d} = {:.2%}".format(index, count, index / count))
-        yield user_bot_kit.get_user(member)
+        yield user_bot_kit.get_user(member, get_bio=True)
 
 
 def export_history(chat_id: int):
