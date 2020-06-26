31b5ec4eaba65f9183cc0589aaa7c74b505edcdb
Septs <github@septs.pw>
2020-06-26 10:15:36 +0800

    Regular update: 2020-06-26T02:15:36Z

--- get-chats.py
+++ get-chats.py
@@ -6,16 +6,39 @@ from pyrogram import Chat, Client, Dialog
 app = Client("my_account")
 
 
+# noinspection PyBroadException
 def get_chats() -> Generator[Chat, None, None]:
+    self_id = app.get_me().id
     for dialog in app.iter_dialogs():
         dialog: Dialog
-        yield dialog.chat
+        chat = dialog.chat
+        status = None
+        try:
+            status = chat.get_member(self_id).status
+        except:
+            pass
+        yield chat, status
+
+
+def display_name(chat: Chat):
+    if chat.title:
+        return chat.title
+    if chat.last_name:
+        return chat.first_name + " " + chat.last_name
+    return chat.first_name
 
 
 def main():
     app.start()
-    for chat in sorted(get_chats(), key=lambda chat: chat.id):
-        print("#%14s | %-20s | %s" % (chat.type, chat.id, chat.title))
+    for chat, status in get_chats():
+        row = "#%14s | %-20s | %-40s | %-13s | %s" % (
+            chat.type,
+            chat.id,
+            chat.username or "",
+            status or "",
+            display_name(chat),
+        )
+        print(row)
     app.stop()
 
 
