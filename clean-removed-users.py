a36a35f8b15a2159cf22a94a1f342d11872a562c
Septs <github@septs.pw>
2020-06-26 09:53:37 +0800

    Regular update: 2020-06-26T01:53:37Z

--- clean-removed-users.py
+++ clean-removed-users.py
@@ -11,6 +11,8 @@ app = Client("my_account")
 def clean_removed_users(chat_id: int):
     remove_member = user_bot_kit.retry(user_bot_kit.users.remove_member)
     for member in app.iter_chat_members(chat_id=chat_id, filter="kicked"):
+        if member.restricted_by.first_name.startswith("SCP-079"):
+            continue
         remove_member(app, chat_id, member)
 
 
