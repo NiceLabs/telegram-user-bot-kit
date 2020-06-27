8cb6e806472e341fbf7820c98949c011d8150889
Septs <github@septs.pw>
2020-06-27 21:28:48 +0800

    Regular update: 2020-06-27T13:28:48Z

--- clean-unable-message.py
+++ clean-unable-message.py
@@ -8,8 +8,11 @@ app = Client("my_account")
 
 
 def is_bot_command(message: Message):
-    if message.text and message.text[0] == "/" and message.entities:
-        return any(entity.type == "bot_command" for entity in message.entities)
+    if message.entities:
+        return any(
+            entity.type == "bot_command" and entity.offset == 0
+            for entity in message.entities
+        )
     return False
 
 
