da2d7e1674b58887bbd5e7aa7f2f79e473e61f14
Septs <github@septs.pw>
2020-06-28 11:24:29 +0800

    Regular update: 2020-06-28T03:24:29Z

--- analysis.py
+++ analysis.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 import sys
 
-from pandas import DataFrame, Series, read_csv
+from pandas import DataFrame, read_csv
+
+
+def analysis(chat_id: int):
+    history_df: DataFrame = read_csv("data/%s-history.csv" % chat_id)
+    members_df: DataFrame = read_csv("data/%s-members.csv" % chat_id)
+
+    grouped_df: DataFrame = history_df.groupby("User ID") \
+        .size() \
+        .reset_index(name="Count") \
+        .merge(members_df, on="User ID", how="inner") \
+        .sort_values("Count", ascending=False)
+    grouped_df.to_csv("data/%s-grouped.csv" % chat_id, index=False)
+
+    keep_df = grouped_df[grouped_df["Count"] > 3]
+    diff = (members_df["Status"] == "member") & \
+           (~members_df["User ID"].isin(keep_df["User ID"]))
+    members_df[diff] \
+        .to_csv("data/%s-removable.csv" % chat_id, index=False)
 
 
 def main():
-    chat_id = int(sys.argv[1])
-    history_df: DataFrame = read_csv("data/%s-history.csv" % chat_id)
-    members_df: DataFrame = read_csv("data/%s-members.csv" % chat_id)
-    members_df = members_df[members_df["Status"] == "member"]
-    merged: DataFrame = history_df.merge(members_df, on="User ID", how="inner")
-    grouped: DataFrame = merged \
-        .groupby("User ID") \
-        .size() \
-        .reset_index(name="Count") \
-        .sort_values("Count", ascending=False)
-    grouped = grouped[grouped["Count"] > 3]
-    grouped.to_csv("data/%s-grouped.csv" % chat_id, index=False)
-
-    removable = Series(list(set(members_df["User ID"]) - set(grouped["User ID"])), name="User ID") \
-        .sort_values(ascending=False)
-    removable.to_csv("data/%s-removable.csv" % chat_id, index=False)
+    for chat_id in sys.argv[1:]:
+        analysis(int(chat_id))
 
 
 if __name__ == "__main__":
