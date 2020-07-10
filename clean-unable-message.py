710419ca627ca926989e834df23649a58b9206e5
Septs <github@septs.pw>
2020-07-10 01:24:06 +0000

    updated

--- clean-unable-message.py
+++ clean-unable-message.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-from datetime import datetime, timedelta
 
 from pyrogram import Client
 
@@ -11,7 +10,6 @@ app = Client("bot")
 
 def remove_unable_message(chat_id: int):
     for message in app.iter_history(chat_id=chat_id):
-        delta: timedelta = datetime.now() - datetime.fromtimestamp(message.date)
         if is_bot_command(message) or message.dice:
             message.delete()
         if not message.service:
